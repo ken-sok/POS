e9100cb3dd1b82423dd6e2fee8cf699775376b57
kheang <kheang.learning@gmail.com>
2020-05-22 11:51:25 +0700

    initial commit

---
+++ readme.txt
@@ -0,0 +1,4 @@
+Login
+
+User: owner
+Pass: owner
---
+++ views/modules/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+
+	<strong>Copyright &copy;<? echo date(Y);?> Servicing you Car Spare Parts</strong>
+	
+	All rights reserved. 
+
+</footer>
